ed6fbbb1f67b96998e110717fb7daf8aece3de4d
steve_hong <stevehong0615@gmail.com>
2016-08-17 14:20:26 +0000

    memcached.php 加入背景執行; client.php讀取memcached 輸出json頁面

--- memcached.php
+++ memcached.php
@@ -1,4 +1,7 @@
 <?php
+ignore_user_abort(true);
+set_time_limit(0);
+
 error_reporting(E_ALL & ~E_NOTICE);
 require_once 'Connect.php';
 
@@ -7,15 +10,12 @@ $mc->addServer("localhost", 11211);
 
 $connect = new Connect;
 
-$selectData = "SELECT * FROM `read_soccer_data`";
-$data = $connect->db->prepare($selectData);
-$data->execute();
-$result = $data->fetchAll(PDO::FETCH_ASSOC);
+while (true) {
+    $selectData = "SELECT * FROM `read_soccer_data`";
+    $data = $connect->db->prepare($selectData);
+    $data->execute();
+    $result = $data->fetchAll(PDO::FETCH_ASSOC);
 
-$mc->set("dataToday", $result);
-
-$arr = array(
-    $mc->get("dataToday"),
-);
-var_dump($arr);
-?>
+    $mc->set("dataToday", $result);
+    sleep(60);
+}
